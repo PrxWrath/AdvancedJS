1f77d4132ea1a9e2e26dc2c7ef42254030fdf6bb
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-10-31 14:36:23 +0530

    Added call back functions files

---
+++ CallBack.js
@@ -0,0 +1,8 @@
+document.getElementById('clicker')
+.addEventListener('click',function(){
+    console.log("Clicked me");
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    console.log("DOM has loaded");
+})
